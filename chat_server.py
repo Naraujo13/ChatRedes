00a2ae69d4f0da3d86797ba26d76ea13fd905d74
Vinicius Furlan <Vinicius.s.furlan@gmail.com>
2017-03-22 01:52:00 -0300

    alteração dos comentários

--- chat_server.py
+++ chat_server.py
@@ -1,98 +1,101 @@
-import socket, select
+imPort socket, select
  
-#Function to broadcast chat messages to all connected clients
-def broadcast_data (sock, message):
-    #Do not send the message to master socket and the client who has send us the message
-    i = -1
-    for nickname, socket in CONNECTION_LIST.items():
+# Função public_message:
+#
+# Envia uma mensagem publica para todos os usuários conectados na sala
+# ignorando o socket do servidor e do usuário que enviou a mensagem
+
+def public_message (sock, message):
+    for nickname, socket in Connected_Sockets.items():
         if socket != server_socket and socket != sock :
             try :
                 socket.send(message)
             except :
-                # broken socket connection may be, chat client pressed ctrl+c for example
                 socket.close()
-                del CONNECTION_LIST.keys()[CONNECTION_LIST.values().index(socket)]
+                del Connected_Sockets.keys()[Connected_Sockets.values().index(socket)]
 
 
-def broadcast_pm(sock, message):
-    # Do not send the message to master socket and the client who has send us the message
-    for nickname, socket in CONNECTION_LIST.items():
+# Função private_message:
+#
+# Envia uma mensagem privada para um usuário conectado na sala
+
+def private_message(sock, message):
+    for nickname, socket in Connected_Sockets.items():
         if socket == sock:
             try:
                 socket.send(message)
             except:
-                # broken socket connection may be, chat client pressed ctrl+c for example
                 socket.close()
-                del CONNECTION_LIST.keys()[CONNECTION_LIST.values().index(socket)]
+                del Connected_Sockets.keys()[Connected_Sockets.values().index(socket)]
 
 
 if __name__ == "__main__":
+
+    # Inicializa o dicionário de sockets conectados e parâmtros relativos aos sockets     
+    Connected_Sockets = {}
+    receiving_buffer = 4096
+    Port = 5000
      
-    # List to keep track of socket descriptors
-    CONNECTION_LIST = {}
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = 5000
-     
+    # Inicializa o socket do servidor
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.bind(("0.0.0.0", Port))
     server_socket.listen(10)
  
-    # Add server socket to the list of readable connections
-    CONNECTION_LIST['server'] = server_socket
+    # Adiciona o socket do servidor ao dicionário de
+    Connected_Sockets['server'] = server_socket
  
-    print "Chat server started on port " + str(PORT)
+    print "UOL Chat started on Port " + str(Port)
  
     while 1:
-        # Get the list sockets which are ready to be read through select
-        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST.values(),[],[])
+
+        # Busca as listas de sockets disponíveis para leitura, escrita e erro.
+        # utiliza os sockets de leitura para leitura das entradas do usuario
+        # e também para recebimento de mensagens
+        read_sockets,write_sockets,error_sockets = select.select(Connected_Sockets.values(),[],[])
  
         for sock in read_sockets:
-            #New connection
             if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
+                # Bloco de código resposável pelo tratamento de uma nova conexão
+                # e após estabelecer a conexão envia uma mensagem avisando que o 
+                # usuário se conectou a sala
                 sockfd, addr = server_socket.accept()
-                nickname = sockfd.recv(RECV_BUFFER)
-                CONNECTION_LIST[nickname] = sockfd
-                print "Client (%s, %s) connected" % addr
-                print "Nickname: " + nickname
-                broadcast_data(sockfd, "\r" + nickname + " entered the room!\n")
+                nickname = sockfd.recv(receiving_buffer)
+                Connected_Sockets[nickname] = sockfd
+                print "Client " + str(addr) + " connected with nickname " + nickname
+                public_message(sockfd, "\r" + nickname + " entered the room!\n")
              
-            #Some incoming message from a client
             else:
-                # Data recieved from client, process it
                 try:
-                    #In Windows, sometimes when a TCP program closes abruptly,
-                    # a "Connection reset by peer" exception will be thrown
-                    data = sock.recv(RECV_BUFFER)
-                    sender = CONNECTION_LIST.keys()[CONNECTION_LIST.values().index(sock)]
+                    #Caso a conexão não seja nova realiza os tratamentos para recebimento e envio de mensagens
+                    data = sock.recv(receiving_buffer)
+                    sender = Connected_Sockets.keys()[Connected_Sockets.values().index(sock)]
                     if '/' not in data :
-                        broadcast_data(sock, "\r" + sender + ": " +  data)
+                        public_message(sock, "\r" + sender + ": " +  data)
                     else:
                         if data == "/list\n":
                             message = "Users in the room:\n"
-                            for nickname in CONNECTION_LIST.keys():
+                            for nickname in Connected_Sockets.keys():
                                 if nickname != "server":
                                     message += nickname + "; "
-                            broadcast_pm(sock, "\r" + message + "\n")
+                            private_message(sock, "\r" + message + "\n")
                         elif data == "/quit\n":
-                            broadcast_data(sock, "\r" + sender + " has left the room!\n")
-                            print "Client (%s, %s) disconnected.\n" % addr
+                            public_message(sock, "\r" + sender + " has left the room!\n")
+                            print "Client " + sender + " " + str(addr) + " disconnected.\n"
                             sock.close()
-                            del CONNECTION_LIST[sender]
+                            del Connected_Sockets[sender]
                         else:
                             nickname = data.split(" ")
                             nickname = nickname[0].replace("/", "")
                             try:
-                                broadcast_pm(CONNECTION_LIST[nickname], "\r\n[PM]" + sender + ": " + data.replace("/" + nickname + " ", "") + "\n")
+                                private_message(Connected_Sockets[nickname], "\r\n[PM]" + sender + ": " + data.replace("/" + nickname + " ", "") + "\n")
                             except:
-                                broadcast_pm(sock, "\rUser not found!\n")
+                                private_message(sock, "\rUser not found!\n")
                 except:
-                    broadcast_data(sock, "Client (%s, %s) is offline" % addr)
-                    print "Client (%s, %s) is offline" % addr
+                    public_message(sock, "Client " + str(addr) + " is offline!")
+                    print "Client " + str(addr) + " is offline!" 
                     sock.close()
-                    del CONNECTION_LIST.keys()[CONNECTION_LIST.values().index(sock)]
+                    del Connected_Sockets.keys()[Connected_Sockets.values().index(sock)]
                     continue
      
     server_socket.close()
--- telnet.py
+++ telnet.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         print 'Unable to connect'
         sys.exit()
      
-    print 'Connected to remote host. Start sending messages'
+    print 'You connected to the UOL Chat!. Have fun!'
     prompt()
      
     while 1:
@@ -44,7 +44,7 @@ if __name__ == "__main__":
             if sock == s:
                 data = sock.recv(4096)
                 if not data :
-                    print '\nDisconnected from chat server'
+                    print '\nDisconnected from UOL Chat!'
                     sys.exit()
                 else :
                     #print data
